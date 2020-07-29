2f9d3eb3f8f06aa9b8f4316ed319db5bc30629a2
tomer <tomer.kubovsky@gmail.com>
2020-07-29 21:30:45 +1000

    sda sfadfdsaas dfafsd dafsadfs
     asdfafsd fsadasfdadsf

---
+++ Assets/Scripts/PlayerScripts/SpinningHeadScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningHeadScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Input.GetAxis("Mouse Y")*-1, 0f, 0f);
+    }
+}
